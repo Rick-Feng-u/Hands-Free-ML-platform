35c90cfbd5a66918a82d4ff4e1d7fd6bd194f7df
Rick Feng <rickfeng99@gmail.com>
2024-12-31 21:46:30 -0500

    start edit config of main hub

--- main_hub/config/transformation_config.py
+++ main_hub/config/transformation_config.py
@@ -1,8 +1,12 @@
 import os
+try:
+    from ml_platform
+from global_files.configs.global_configs import
 
 env = os.getenv('ENV', 'exp')
 
 class TransformationConfig:
     def __init__(self, **kwargs):
+
         self.weekly_interval_transformhub: str = "30 4 * * 6"
         
