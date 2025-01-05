be1521feee00c9b9995bff51814cde560c228c9c
Rick Feng <rickfeng99@gmail.com>
2025-01-05 16:45:04 -0500

    started util for main hub

---
+++ main_hub/util/transformation_util.py
@@ -0,0 +1,16 @@
+import json
+from datetime import datetime, date
+from airflow.models import Variable
+
+try:
+    from ml_platform.main_hub.configs.transformation_config import (TransformationConfigs,)
+    from ml_platform.main_hub.operators.transformation_operators import (
+        TransformSubmitPySparkJobOperator
+    )
+except ImportError:
+    from main_hub.config.transformation_config import (TransformationConfigs,)
+    from main_hub.operators.transformation_operators import (
+        TransformSubmitPySparkJobOperator
+    )
+
+transform_config = TransformationConfigs()
